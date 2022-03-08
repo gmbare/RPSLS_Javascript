62f3f4a1fb57ad2f56166b431b1a5d57b6a9e196
Garett Bare <Garett.Bare@yahoo.com>
2022-03-07 21:36:19 -0600

    Basic functionality for determining the winner of the game based off of best to 3

--- game.js
+++ game.js
@@ -12,30 +12,31 @@ Paper disproves Spock 
 Spock vaporizes Rock   
 */
 
-const prompt = require('prompt-sync')
+const prompt = require('prompt-sync')();
 const human = require('./human.js');
 const computer = require('./computer.js');
+const player = require('./player.js');
 
 
 class game {
     constructor() {
-        this.playerOne = new human('Player')
-        this.playerTwo = new computer('Computer')
+        this.playerOne = new human('Player 1');
     }
 
-    playerChoice() {
-        console.log("Please choose a hand\n0=Rock\n1=Paper\n2=Scissors\n3=Lizard\n4=Spock\n")
-        return(`${this.playerOne.name} played ${this.playerOne.chooseGesture().name}`)
+    playerChoice(player) {
+        console.log(`${player.name} : Please choose a hand\n0=Rock\n1=Paper\n2=Scissors\n3=Lizard\n4=Spock\n`)
+        return (player.chooseGesture())
+        // return(`${player.name} played ${player.chooseGesture().name}`)
     }
 
-    robotChoice(){
-        return(`${this.playerTwo.name} played ${this.playerTwo.hand.name}`)
+    robotChoice() {
+        return (`${this.playerTwo.name} played ${this.playerTwo.hand.name}`)
     }
 
-    chooseGame(){
+    chooseGame() {
         console.log("Would you like to play against another player or computer?\nSelect 1 for player\nSelect 2 for computer.");
-        let userChoice = prompt("");
-        switch (userChoice){
+        this.userChoice = prompt("");
+        switch (this.userChoice) {
             case '1':
                 this.playerTwo = new human('Player 2');
                 break;
@@ -48,7 +49,28 @@ class game {
         }
     }
 
+    roundWinner() {
+        this.round = 0
+        while((this.playerOne.count < 2) && (this.playerTwo.count < 2)) {
+            this.playerOne.hand = this.playerChoice(this.playerOne)
+            this.playerTwo.hand = this.playerChoice(this.playerTwo)
+            this.round ++
+            if (this.playerOne.hand.weakness.includes(this.playerTwo.hand.name)) {
+                this.playerTwo.count++;
+                console.log(`Player Two is the winner of round ${this.round}`)
+            }
+            else if (this.playerTwo.hand.weakness.includes(this.playerOne.hand.name)) {
+                this.playerOne.count++;
+                console.log(`Player One is the winner of round ${this.round}`)
+            }
+            else {
+                console.log(`Round ${this.round} ends in a tie\n`)
+            }
+        }
+        return(this.playerOne.count>this.playerTwo.count ? 'Player One is the Winner':'Player Two is the winner')
+    }
+
 }
 
 
-module.exports= game
+module.exports = game
--- main.js
+++ main.js
@@ -4,3 +4,10 @@ const game = require('./game.js')
 let mainGame = new(game)
 
 console.log(mainGame.chooseGame());
+// console.log(`${mainGame.playerChoice(mainGame.playerOne).name} \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n`);
+// console.log(`${mainGame.playerChoice(mainGame.playerTwo).name} \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n`);
+
+// console.log(`${mainGame.playerOne.name} played ${mainGame.playerOne.hand.name}`)
+// console.log(`${mainGame.playerTwo.name} played ${mainGame.playerTwo.hand.name}`)
+
+console.log(`\n${mainGame.roundWinner()}`)
