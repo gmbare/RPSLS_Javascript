bbd52eb1ace8233bdd126e18f32ee6a554030eb7
Garett Bare <Garett.Bare@yahoo.com>
2022-03-07 19:38:06 -0600

    init

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict";
+/*
+Rock crushes Scissors    
+Scissors cuts Paper   
+Paper covers Rock   
+Rock crushes Lizard   
+Lizard poisons Spock   
+Spock smashes Scissors   
+Scissors decapitates Lizard   
+Lizard eats Paper   
+Paper disproves Spock   
+Spock vaporizes Rock   
+*/
